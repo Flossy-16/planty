6671c2a8fd3f378195d312de55eec9194e03a71a
Flossy Neveu <neveu.flossy@gmail.com>
2023-12-22 17:22:55 +0100

    changement du theme et intégration du contenu

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra Child
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function theme_enqueue_styles() {
+
+	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+	wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 15 );
