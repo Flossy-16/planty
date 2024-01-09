dd57456af6997d34d2395b8909cb4daefb3f3282
Flossy Neveu <neveu.flossy@gmail.com>
2024-01-09 12:19:25 +0100

    ajout de commentaires

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -19,7 +19,8 @@ define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
 function theme_enqueue_styles() {
 
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-	wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+	// ajout wp_enqueue_style pour charger le css du fichier theme.css
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 15 );
